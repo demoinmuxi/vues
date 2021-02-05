aa18e66d17f7f83dea5aa9516fe28aad68130211
vikiteu <kiteu@qq.com>
2021-02-05 20:03:55 +0800

    p

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,37 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <p v-for="i in texts" :key="i">{{i}}</p>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
-
+function getTexts(){
+  return new Promise(reslove=>{
+    setTimeout(()=>{
+      reslove(['t1','t2','t3']);
+    },2000);
+  })
+}
 export default {
   name: 'App',
+  data(){
+    return {
+      texts:[],
+    }
+  },
+  async created() {
+    //组件示例已创建未挂载dom不存在
+    await getTexts();
+  },
+  mounted(){
+    // 已挂载dom存在
+  },
   components: {
     HelloWorld
+  },
+  watch:{
+
   }
 }
 </script>
